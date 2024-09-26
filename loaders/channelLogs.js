b20ec6b748a75822ae6d3c9962a5de0848d58ba1
BaptisteRobles <baptiste.robles@epitech.eu>
2024-09-26 13:43:45 +0200

    [+]fix_timezone

--- loaders/channelLogs.js
+++ loaders/channelLogs.js
@@ -1,12 +1,13 @@
 const { EmbedBuilder } = require('discord.js');
+const moment = require('moment-timezone');
 
 module.exports = function(client, message) {
     const channelId = '1288436665206968370';
     const channel = client.channels.cache.get(channelId);
 
     if (channel) {
-        const now = new Date();
-        const time = now.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        const now = moment().tz('Europe/Paris');
+        const time = now.format('HH:mm:ss');
         const embed = new EmbedBuilder()
             .setColor('#FF0000')
             .setDescription(`【${time}】 ${message}`);
